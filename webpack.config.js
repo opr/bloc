e2536883d1120fe6f7d0122bed56ea2ef6c1d7fa
Thomas Roberts <celeissl@gmail.com>
2019-05-09 15:45:43 +0100

    ssr sorted and fonts fixed

--- webpack.config.js
+++ webpack.config.js
@@ -8,85 +8,67 @@ const SassLintPlugin = require('sass-lint-webpack');
 const postcssPresetEnv = require('postcss-preset-env');
 
 
-module.exports = (env, argv) => ({
-  devServer: {
-    port: 3000,
-    //comment this out to not use with another back end like iis
-    /*proxy: {
-      path: '/',
-      target: 'http://127.0.0.1:5080'
-    },*/
-    host: '0.0.0.0'
-  },
-  optimization: {
-    minimizer: argv.mode === 'development' ? [] : [new TerserPlugin({test: /\.js/}), new OptimizeCSSAssetsPlugin({})],
-  },
-  plugins: [
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new MiniCssExtractPlugin({
-      filename: 'bloc.min.css',
-    }),
-    new SassLintPlugin()
-  ],
-  entry: [
-    './assets/js/src/react/index.jsx',
-    './assets/js/src/modules/index.js',
-    './assets/styles/scss/bloc.scss'
-  ],
-  output: {
-    path: path.join(__dirname, 'assets', 'dist'),
-    filename: 'bloc.min.js',
-    publicPath: '/assets/dist'
-  },
-  resolve: {
-    alias: {'react-dom': '@hot-loader/react-dom'}
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(jsx?)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        },
-        resolve: {
-          extensions: ['.js', '.jsx']
+module.exports = (env, argv) => {
+  console.log(argv.mode);
+  return {
+    devServer: {
+      port: 3000,
+      //comment this out to not use with another back end like iis
+      /*proxy: {
+        path: '/',
+        target: 'http://127.0.0.1:5080'
+      },*/
+      host: '0.0.0.0'
+    },
+    optimization: {
+      minimizer: argv.mode === 'development' ? [] : [new TerserPlugin({
+        test: /\.jsx?/,
+        terserOptions: {
+          compress: {
+            drop_console: true
+          }
         }
-      },
-      {
-        enforce: 'pre',
-        test: /\.jsx?$/,
-        exclude: /node_modules|.*vendor.*/,
-        loader: 'eslint-loader'
-      },
-      {
-        test: /\.(sc|sa|c)ss$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
+      })],
+      minimize: argv.mode !== 'development'
+    },
+    plugins: [
+      new webpack.optimize.OccurrenceOrderPlugin(),
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify('production'),
+        'process.env.SERVER_SIDE': JSON.stringify(true)
+      }),
+      new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
+    ],
+    entry: [
+      './assets/js/src/serverSideReact/index.jsx',
+    ],
+    output: {
+      path: path.join(__dirname, 'assets', 'dist'),
+      filename: 'bloc.serverSide.min.js',
+      publicPath: '/assets/dist'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(jsx?)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
             options: {
-              hmr: argv.mode === 'development',
+              envName: argv.mode === 'development' ? 'development' : 'production'
             }
           },
-          {loader: 'css-loader', options: {sourceMap: true, url: false}},
-          {
-            loader: 'postcss-loader',
-            options: {
-              config: {
-                path: path.join(__dirname, 'postcss.config.js')
-              },
-              plugins: () => [postcssPresetEnv()]
-            }
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              importer: globImporter()
-            }
-          },
-        ],
-      }
-    ]
-  }
-});
+          resolve: {
+            extensions: ['.js', '.jsx']
+          }
+        },
+        {
+          enforce: 'pre',
+          test: /\.jsx?$/,
+          exclude: /node_modules|.*vendor.*/,
+          loader: 'eslint-loader'
+        }
+      ]
+    }
+  };
+};
