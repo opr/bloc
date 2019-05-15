1ebc95a111734f28487bc58077a2ba05d475c2cd
Thomas Roberts <celeissl@gmail.com>
2019-05-15 18:21:34 +0100

    turned hot loading plugin off

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ module.exports = (env, argv) => ({
   },
   plugins: [
     new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
+    //new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(argv.mode === 'development' ? 'development' : 'production'),
       'process.env.BABEL_ENV': JSON.stringify(argv.mode === 'development' ? 'development' : 'production')
